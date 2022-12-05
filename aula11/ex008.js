194d73410b9ef9aec3100c314aace3a3226d9969
FelipeTebas <faraujodossantos@rede.ulbra.br>
2022-12-05 07:10:36 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`Você está a ${vel}km/h!`)
+if (vel > 60){
+    console.log(`Você está a ${vel - 60}km/h acima da velocidade permitida! MULTADO!!!`)
+}
+console.log(`coloque o cinto, dirija com segurança!`)
