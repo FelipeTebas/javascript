aaafe8cb9d817960c8ee63cc2fb7f207e9666da5
FelipeTebas <faraujodossantos@rede.ulbra.br>
2022-12-18 15:23:59 -0300

    ajustes

--- aula12ex/ex015/scriptex15.js
+++ aula12ex/ex015/scriptex15.js
@@ -17,37 +17,39 @@ function carregar() {
     
     if (iano.value.length == 0 || iano.value > hoje){
         alert('[ERRO] Verifique os dados e tente novamente.')
+    }else{
+        if (genero == 'Homem'){
+            if (idade >= 0 && idade<= 10){
+                foto.src = 'bebe_h.png'
+                texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+            }else if (idade < 35){
+                foto.src = 'jovem_h.png'
+                texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+            }else if (idade < 65){
+                foto.src = 'adulto_h.png'
+                texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+            }else {
+                foto.src = 'idoso_h.png'
+                texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+            }
+        }else {
+            if (idade >= 0 && idade<= 10){
+                foto.src = 'bebe_m.png'
+                texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+            }else if (idade < 35){
+                foto.src = 'jovem_m.png'
+                texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+            }else if (idade < 65){
+                foto.src = 'adulto_m.png'
+                texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+            }else {
+                foto.src = 'idoso_m.png'
+                texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+            }
+        }
     }
 
-    if (genero == 'Homem'){
-        if (idade >= 0 && idade<= 10){
-            foto.src = 'bebe_h.png'
-            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
-        }else if (idade < 35){
-            foto.src = 'jovem_h.png'
-            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
-        }else if (idade < 65){
-            foto.src = 'adulto_h.png'
-            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
-        }else {
-            foto.src = 'idoso_h.png'
-            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
-        }
-    }else {
-        if (idade >= 0 && idade<= 10){
-            foto.src = 'bebe_m.png'
-            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
-        }else if (idade < 35){
-            foto.src = 'jovem_m.png'
-            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
-        }else if (idade < 65){
-            foto.src = 'adulto_m.png'
-            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
-        }else {
-            foto.src = 'idoso_m.png'
-            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
-        }
-    }
+    
 
 
 }
