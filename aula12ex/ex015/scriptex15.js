f3cb692543d7e535711c7cc8c3570055c85c0ca7
FelipeTebas <faraujodossantos@rede.ulbra.br>
2022-12-18 12:00:47 -0300

    exercício verificação de idade finalizado

--- aula12ex/ex015/scriptex15.js
+++ aula12ex/ex015/scriptex15.js
@@ -1,4 +1,53 @@
 function carregar() {
+
+    var data = new Date()
+    var hoje = data.getFullYear()
     var nasc = document.getElementById('iano')
+    var idade = hoje - nasc.value
+    var texto = document.getElementById('ridade')
+    var foto = document.getElementById('img')
     var sex = document.getElementsByName('sexo')
+    var genero = ''
+
+    if (sex[0].checked){
+        genero = 'Homem'
+    }else{
+        genero = "Mulher"
+    }
+    
+    if (iano.value.length == 0 || iano.value > hoje){
+        alert('[ERRO] Verifique os dados e tente novamente.')
+    }
+
+    if (genero == 'Homem'){
+        if (idade <= 15){
+            foto.src = 'bebe_h.png'
+            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+        }else if (idade < 35){
+            foto.src = 'jovem_h.png'
+            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+        }else if (idade < 65){
+            foto.src = 'adulto_h.png'
+            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+        }else {
+            foto.src = 'idoso_h.png'
+            texto.innerHTML = `Detectamos um homem de ${idade} anos.`
+        }
+    }else {
+        if (idade <= 15){
+            foto.src = 'bebe_m.png'
+            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+        }else if (idade < 35){
+            foto.src = 'jovem_m.png'
+            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+        }else if (idade < 65){
+            foto.src = 'adulto_m.png'
+            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+        }else {
+            foto.src = 'idoso_m.png'
+            texto.innerHTML = `Detectamos uma mulher de ${idade} anos.`
+        }
+    }
+
+
 }
