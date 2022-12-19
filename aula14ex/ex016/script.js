e519357e1dfd9e5fa4d2372d2cc3018e6a9bc941
FelipeTebas <faraujodossantos@rede.ulbra.br>
2022-12-19 10:13:40 -0300

    exercício 16 iniciado

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,11 @@
+function verificar() {
+    var numi = document.getElementById('ini')
+    var numf = document.getElementById('fim')
+    var nump = document.getElementById('passo')
+
+    if (numi.value.length == 0  || numf.value.length == 0 || nump.value.length == 0) {
+        alert('[ERRO] verifique as informações')
+    }else {
+        
+    }
+}
